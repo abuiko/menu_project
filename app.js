103bed7b5e8f22d16acec9e773205398aa5d3172
Alena Buiko <abuiko.ma@gmail.com>
2021-03-05 12:48:15 -0500

    add filter btns

--- app.js
+++ app.js
@@ -87,11 +87,16 @@ const menu = [
 
 ]
 
+const sectionCenter = document.querySelector('.section-center');
 
 
 window.addEventListener('DOMContentLoaded', () => {
-    const sectionCenter = document.querySelector('.section-center');
-    let displayMenu = menu.map(item => {
+    displayMenuItems(menu);
+});
+
+function displayMenuItems(menuItems) {
+
+    let displayMenu = menuItems.map(item => {
         return `<article class="menu-item">
        <img src=${item.img} class="photo" alt="${item.img}">
 
@@ -108,5 +113,4 @@ window.addEventListener('DOMContentLoaded', () => {
 
     displayMenu = displayMenu.join('');
     sectionCenter.innerHTML = displayMenu;
-    console.log(sectionCenter);
-})
+}
