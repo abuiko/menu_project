457afb48fc9d27749b630c51b8591146c3f1ef19
Alena Buiko <abuiko.ma@gmail.com>
2021-03-05 11:58:15 -0500

    create menu array with menu items inside

---
+++ app.js
@@ -0,0 +1,77 @@
+const menu = [
+
+    {
+        id: 1,
+        title: 'French Croissant',
+        img: './img/croissant.jpg',
+        price: '8.99$',
+        category: 'breakfast',
+        info: ''
+    },
+    {
+        id: 2,
+        title: 'Berry Cupcake',
+        img: './img/cupcake.jpg',
+        price: '5.69$',
+        category: 'breakfast',
+        info: ''
+    },
+    {
+        id: 3,
+        title: 'Italian Pasta',
+        img: './img/pasta.jpg',
+        price: '17.99$',
+        category: 'dinner',
+        info: ''
+    },
+    {
+        id: 4,
+        title: 'Fresh Salad',
+        img: './img/salad.jpg',
+        price: '10.29$',
+        category: 'lunch',
+        info: ''
+    },
+    {
+        id: 5,
+        title: 'Cesar Salad',
+        img: './img/salad2.jpg',
+        price: '12.59$',
+        category: 'lunch',
+        info: ''
+    },
+    {
+        id: 6,
+        title: 'Tom Yum Soup',
+        img: './img/soup.jpg',
+        price: '4.99$',
+        category: 'lunch',
+        info: ''
+    },
+
+    {
+        id: 7,
+        title: 'Beef Steak',
+        img: './img/steak.jpg',
+        price: '20.99$',
+        category: 'dinner',
+        info: ''
+    },
+    {
+        id: 8,
+        title: 'Breakfast Toast',
+        img: './img/toast.jpg',
+        price: '4.99$',
+        category: 'breakfast',
+        info: ''
+    },
+    {
+        id: 9,
+        title: 'Lunch Bowl',
+        img: './img/bowl.jpg',
+        price: '11.99$',
+        category: 'lunch',
+        info: ''
+    },
+
+]
